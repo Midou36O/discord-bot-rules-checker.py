97b02dd7e359266a641a16ad7e6d2948876db67d
Midou36O <45198486+Midou36O@users.noreply.github.com>
2020-07-09 16:36:56 +0100

    Update bot_test.py

--- bot_test.py
+++ bot_test.py
@@ -11,13 +11,17 @@ bot.remove_command('help') # Here we disable the default help command
 async def on_message(msg): # msg is the message
     if msg.author == bot.user: # Here we make sure that the bot doesn't check his own messages
         return
-    else:
-        if msg.content.startswith('cactuses'): # Here we look if the message starts with a specific word, like a prefix
-          if msg.content[9:].startswith('explodes'): # 8 is the amount of characters in the first word. If you want to add a space between the two words, remember to add the space in the number too
-            if msg.content[18:].startswith('balloons'): # for 7, it's the same thing as second word
-              await msg.author.add_roles(discord.utils.get(msg.guild.roles, id=680422513121034310)) # add_roles is the call function for adding roles, of course, however we don't have any role object specified, so we make it on the fly by using this cool function that will search for something from a parameter of the wanted object. I recommend to use the ID because it never changes, however you can use name='name' too if you want. I think that other parameters such as 'color' work too, but I never tried, and the docs are shitty on explaining extra stuff
-    await bot.process_commands(msg) # This is a call that you have to use because if on_message is called, bot will ignore every command unless you use this call that makes the bot check if the message was actually a command or not
-
+     else:
+        if msg.content.startswith('cactus'):
+          if msg.content[7:].startswith('explodes'):
+            if msg.content[16:].startswith('baloons'):
+              await msg.author.add_roles(discord.utils.get(msg.guild.roles, id=680422513121034310))
+              await msg.delete()
+        if msg.channel.id == 730161129992618043:
+            w=['!verify','hi','gay', 'owo', 'hewwo'] #actually this is like the blacklist words, put them if ppl says the same thing
+            if msg.content.lower() in w or len(msg.split()) < 4 or len(msg.content) > 1000: #if the messages is lower than 4 words and higher than 1000, delete
+            await msg.delete ()
+    await bot.process_commands(msg)
 bot.run('NzMwMTMyNzM2Mzk5OTAwNzAz.XwTDIA.L-OSjhi_HH2m4Y60xbFR6xCUlxs')
 
 
